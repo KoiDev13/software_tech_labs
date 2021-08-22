43c316740b9c5116d7995cc3c146e9baa75fea0c
khoinguyena <edragon.master@live.com>
2021-08-22 23:01:05 +0700

    Update Lab02

--- Lab02/Demo02.js
+++ Lab02/Demo02.js
@@ -1,12 +1,16 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostName = 'localhost';
 const port = 3000;
 
+const filePath = __dirname + '/resources/data.json';
+
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type0', 'text/html');
-    res.end('<h1>Hello World </h1>');
+   let content = fs.readFileSync(filePath, 'utf-8');
+   res.statusCode = 200;
+   res.setHeader('Content-Type0', 'text/json');
+   res.end(content);
 });
 
 server.listen(port, hostName, ()=>{
---
+++ Lab03/Demo01.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const hostName = 'localhost';
+const port = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end("GET method");
+});
+
+app.post('/', (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end("POST method");
+});
+
+app.put('/', (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end("PUT method");
+});
+
+app.delete('/', (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end("DELETE method");
+});
+
+app.listen(port, hostName, () => {
+    console.log(`Server is running on http://${hostName}:${port}`);
+})
